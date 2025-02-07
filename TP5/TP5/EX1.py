00acfbed36548a35b678fcb89223ed7c8e7e74cc
khawlaelhassnaoui639 <khawlaelhassnaoui1@icloud.com>
2025-02-07 18:30:52 +0100

    Delete TP5/TP5 directory

--- TP5/TP5/EX1.py
+++
@@ -1,8 +0,0 @@
-# Ouvrir le fichier en mode lecture
-with open("exemple.txt", "r", encoding="utf-8") as fichier:
-    # Lire les lignes du fichier
-    lignes = fichier.readlines()
-
-# Parcourir et afficher chaque ligne avec son numéro
-for numero, ligne in enumerate(lignes, start=1):
-    print(f"Ligne {numero}: {ligne.strip()}")
